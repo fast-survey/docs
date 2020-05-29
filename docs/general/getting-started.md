eb896d54d0699b609854dc5c36efd97285e0a705
Moritz Makowski <moritz@dostuffthatmatters.dev>
2020-05-30 00:49:08 +0200

    Finish general documentation

--- docs/general/getting-started.md
+++
@@ -1,6 +0,0 @@
----
-id: getting-started
-title: Getting Started
----
-
-This is a link to [another document.](../v0/doc3.md) This is a link to an [external page.](http://www.example.com)
--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,5 @@
 module.exports = {
   someSidebar: {
-    "General Information": ["general/getting-started", "general/versions"],
-    "Version 0": ['v0/doc1', 'v0/doc2', 'v0/doc3'],
+    "General Information": ["general/getting-started", "general/versions", "general/development"],
   },
 };
