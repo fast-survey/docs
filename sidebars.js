0f9df6d5b03529b6d2d700b3213be2df36bbd648
Moritz Makowski <moritz@dostuffthatmatters.dev>
2020-06-09 23:38:40 +0200

    Finish email submission concept

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,9 @@
 module.exports = {
   someSidebar: {
     "General Information": ["general/getting-started", "general/versions", "general/development"],
-    "Concepts": ["concepts/survey-submission", "concepts/system-architecture"],
+    "Concepts": ["concepts/email-verification", "concepts/system-architecture"],
   },
 };
+
+
+
