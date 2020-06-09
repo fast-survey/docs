8ba6eef4bae2aa06d33f74155fd24ab547fe6903
Moritz Makowski <moritz@dostuffthatmatters.dev>
2020-06-10 01:38:38 +0200

    Add doc sections
    New sections:
    * json-configuration
    * admin-dashboard
    * form-input-types
    * self-hosting

--- sidebars.js
+++ sidebars.js
@@ -2,6 +2,10 @@ module.exports = {
   someSidebar: {
     "General Information": ["general/getting-started", "general/versions", "general/development"],
     "Concepts": ["concepts/email-verification", "concepts/system-architecture"],
+    "JSON Configuration": ["json-configuration/intro"],
+    "Admin Dashboard": ["admin-dashboard/intro"],
+    "Form Input Types": ["form-input-types/intro"],
+    "Self Hosting": ["self-hosting/intro"],
   },
 };
 
