a05b9c2068ec908640a2e1f80ced2f59c7daf52a
Felix Böhm <felix@felixboehm.dev>
2020-09-08 17:05:08 +0200

    Add option and text specifications

--- sidebars.js
+++ sidebars.js
@@ -1,10 +1,9 @@
 module.exports = {
-  someSidebar: {
+  sidebar: {
     "General Information": ["general/getting-started", "general/nomenclature", "general/about-us"],
     "User Guide": ["user-guide/email-verification"],
     "Admin Guide": ["admin-guide/admin-dashboard", "admin-guide/json-configuration-files"],
-    "Field Types": ["field-types/selection", "field-types/radio"],
+    "Field Types": ["field-types/option", "field-types/radio", "field-types/selection", "field-types/text"],
     "Self Hosting": ["self-hosting/introduction", "self-hosting/system-architecture"],
   },
 };
-
