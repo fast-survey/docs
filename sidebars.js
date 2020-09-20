0fe29860ab22cc85256045c932ad6d3464a9cbf1
Felix Böhm <felix@felixboehm.dev>
2020-09-20 15:01:39 +0200

    Integrate and improve Email field entry

--- sidebars.js
+++ sidebars.js
@@ -3,7 +3,7 @@ module.exports = {
     "General Information": ["general/getting-started", "general/nomenclature", "general/about-us"],
     "User Guide": ["user-guide/email-verification"],
     "Admin Guide": ["admin-guide/admin-dashboard", "admin-guide/json-configuration-files"],
-    "Field Types": ["field-types/option", "field-types/radio", "field-types/selection", "field-types/text"],
+    "Field Types": ["field-types/email", "field-types/option", "field-types/radio", "field-types/selection", "field-types/text"],
     "Self Hosting": ["self-hosting/introduction", "self-hosting/system-architecture"],
   },
 };
